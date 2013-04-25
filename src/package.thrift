f7ae3f799f449dfa559d686125b94b7a91296563
Matt Tierney <tierney@cs.nyu.edu>
2013-04-25 14:01:05 -0400

    reorge

--- src/package.thrift
+++ src/package.thrift
@@ -5,9 +5,41 @@ enum PackageType {
   DELTA,
 }
 
-struct Package {
-  1: required string path,
-  2: required string hash,
+struct LocalPackage {
+  1: required string base_abs_path,
+  2: required string base_rel_path,
   3: optional PackageType type,
   4: optional string contents,
+}
+
+struct RemotePackage {
+  1: required string base_abs_path,
+  2: required HybridCrypto path,
+  3: required HybridCrypto data,
+}
+
+struct HybridCrypto {
+  1: required string data,
+  2: optional map<string, string> user_enc_session,
+}
+
+struct PathLock {
+  1: required string base_abs_path,
+  2: required string user,
+  3: required i32 seconds_requested,
+}
+
+# When a user requests an update list for files to fetch, this is the list that
+# the user can use to fetch requests for new downloads.
+struct Updates {
+  1: required string device,
+  2: required string user,
+  3: optional list<HybridCrypto> updates,
+}
+
+service Package Exchange {
+  bool lock(1:PathLock lock),
+  bool upload(1:LocalPackage pkg),
+  bool download(),
+  bool poll(),
 }
