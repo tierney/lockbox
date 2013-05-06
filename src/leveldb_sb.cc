bff68e917bde47fecc65e807813c63281eeaae24
Matt Tierney <tierney@cs.nyu.edu>
2013-05-06 11:26:21 -0400

    leveldb prefix example

--- src/leveldb_sb.cc
+++ src/leveldb_sb.cc
@@ -1,6 +1,8 @@
 #include <cassert>
+#include <iostream>
 #include <string>
 #include "leveldb/db.h"
+#include "base/string_util.h"
 
 using std::string;
 using std::to_string;
@@ -15,6 +17,20 @@ int main(int argc, char** argv) {
   for (int i = 0; i < 10; i++) {
     string to_put("ZZZZ KEY");
     db->Put(leveldb::WriteOptions(), to_put + to_string(i), to_string(10 - i));
+  }
+
+
+  for (int i = 0; i < 10; i++) {
+    string to_put("AAAA KEY");
+    db->Put(leveldb::WriteOptions(), to_put + to_string(i), to_string(10 - i));
+  }
+
+
+  leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+  for (it->Seek("AAAA");
+       it->Valid() && StartsWithASCII(it->key().ToString(), "AAAA", true);
+       it->Next()) {
+    std::cout << it->key().ToString() << std::endl;
   }
 
   delete db;
